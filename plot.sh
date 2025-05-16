34b623410e20cd4dd8d27ec1ed49a82e371d86cb
Elias Jääsaari <elias.jaasaari@gmail.com>
2025-05-16 09:43:29 +0300

    Add plot.sh

---
+++ plot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+local_mode="false"
+args="$@"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --local)
+        local_mode="true"
+        ;;
+    esac
+    shift
+done
+
+if [ $local_mode = "false" ]; then
+    singularity exec "plot.sif" python3 export_results.py
+    singularity exec "plot.sif" python3 plot.py $args
+else
+    python3 export_results.py
+    python3 plot.py $args
+fi
