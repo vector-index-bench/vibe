a5c55d535e5fff15b179c0ad7da3e8023829e021
Elias Jääsaari <elias.jaasaari@gmail.com>
2026-07-28 21:55:38 +0300

    Increase default timeout to 24 hours

--- vibe/main.py
+++ vibe/main.py
@@ -174,7 +174,7 @@ def parse_arguments() -> argparse.Namespace:
         "--timeout",
         type=int,
         help="Timeout (in seconds) for each individual algorithm run, or -1 if no timeout should be set",
-        default=4 * 3600,
+        default=24 * 3600,
     )
     parser.add_argument(
         "--local",
